63c49293fadda5eaa25c02b1dc83aa97bb034a3f
Daniel Yuschick <daniel.yuschick@gmail.com>
2023-03-05 09:48:11 +0200

    refactor: adjust package and entrypoint

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const useLogicalPropertiesAndValues = require("./rules/use-logical-properties-and-values");
+const useLogicalUnits = require("./rules/use-logical-units");
+
+module.exports = {
+  useLogicalPropertiesAndValues,
+  useLogicalUnits,
+};
